97af13c5edfdc5b50a3ce73a2ecfe22921cf0957
Elina Semenko <lavrextra@gmail.com>
2023-05-21 14:17:52 +0200

    Add iOS Framework

---
+++ SimpleImageProcessing.framework/Headers/SimpleImageProcessing.h
@@ -0,0 +1,18 @@
+//
+//  SimpleImageProcessing.h
+//  SimpleImageProcessing
+//
+//  Created by Elina Semenko on 21.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleImageProcessing.
+FOUNDATION_EXPORT double SimpleImageProcessingVersionNumber;
+
+//! Project version string for SimpleImageProcessing.
+FOUNDATION_EXPORT const unsigned char SimpleImageProcessingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleImageProcessing/PublicHeader.h>
+
+
